48e71292464be0cc28872e1d959ce10740640f93
Adeshina Adenuga <nuga0909@gmail.com>
2023-01-19 12:28:10 +0100

    Add styling to popup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './style.css';
 
 const add = document.querySelector('.p-add');
 const popup = document.querySelector('.popup')
+
 add.addEventListener('click', () => {
   popup.classList.toggle('active');
 });
