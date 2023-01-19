936df1d6bc72ff62f5f5b96798271a95f6d3fa05
Adeshina Adenuga <nuga0909@gmail.com>
2023-01-19 05:01:37 +0100

    make popup appear

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 
+const add = document.querySelector('.p-add');
+const popup = document.querySelector('.popup')
+add.addEventListener('click', () => {
+  popup.classList.toggle('active');
+});
