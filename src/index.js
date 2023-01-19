93b14ce4c4ead6ad29947076f709e421918e6e5f
Adeshina Adenuga <nuga0909@gmail.com>
2023-01-19 12:42:59 +0100

    add cursor style

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import './style.css';
 
 const add = document.querySelector('.p-add');
 const popup = document.querySelector('.popup')
+const cancel = document.querySelector('.cancel')
 
 add.addEventListener('click', () => {
   popup.classList.toggle('active');
 });
+cancel.addEventListener('click', () => {
+  popup.classList.remove('active');
+});
+
